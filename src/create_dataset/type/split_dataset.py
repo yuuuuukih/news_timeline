185582a1ea804b07758c00b0157951738088f43c
YuukiHaraProgramming <hara@nlp.ist.i.kyoto-u.ac.jp>
2023-12-17 03:04:09 +0900

    Got the AplitDataset type correct

--- src/create_dataset/type/split_dataset.py
+++ src/create_dataset/type/split_dataset.py
@@ -1,12 +1,14 @@
 from typing import TypedDict
 
-from create_dataset.type.no_fake_timelines import EntityTimelineData
+from create_dataset.type.no_fake_timelines import EntityTimelineData, Setting
 
 class SplitDataset(TypedDict):
     name: str
     description: str
     entities_num: int
-    setting: str
+    timelines_num: int
+    split_n: int
+    setting: Setting
     data: list[EntityTimelineData]
 
 class OneComInfo(TypedDict):
